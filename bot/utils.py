450f59b2af7996af2a97908608e48e5be99084b6
Andrey Sadkov <45257876+nontypeable@users.noreply.github.com>
2023-08-23 13:45:41 +0300

    add: method that makes it easier to work with multiple locales

---
+++ bot/utils.py
@@ -0,0 +1,14 @@
+import gettext
+import os
+from typing import Callable
+
+
+def get_translation(language: str) -> Callable[..., str]:
+    """method that makes it easier to work with multiple locales"""
+
+    # defining an absolute path to locales
+    locales_path = os.path.join(os.path.dirname(__file__), "locales")
+
+    translation = gettext.translation(domain="base", localedir=locales_path, languages=[language], fallback=True)
+
+    return translation.gettext
