c24658148622621767024447f886a9d047f77bc7
Andrey Sadkov <45257876+nontypeable@users.noreply.github.com>
2023-08-23 13:57:56 +0300

    add: methods that handle the language settings of the bot

--- bot/handlers/user/register_all_user_handlers.py
+++ bot/handlers/user/register_all_user_handlers.py
@@ -1,10 +1,11 @@
 from aiogram import Dispatcher
 
 from bot.handlers.user.start import router as start_router
+from bot.handlers.user.settings.language import router as language_settings_router
 
 
 def register_all_user_handlers(dp: Dispatcher) -> None:
     """method that registers all user handlers"""
 
     dp.include_router(start_router)
-
+    dp.include_router(language_settings_router)
---
+++ bot/handlers/user/settings/language.py
@@ -0,0 +1,37 @@
+from aiogram import Router, F, types
+
+from bot.db.methods.fetch import fetch
+from bot.db.methods.update import update
+from bot.utils import get_translation
+
+router = Router()
+
+
+@router.callback_query(F.data == "russian")
+async def set_russian_language(callback: types.CallbackQuery) -> None:
+    """method that sets the Russian language for a user in the database"""
+
+    update(message=callback.message, language="ru")
+
+    # query to get user language from database
+    fetch_language_query = """SELECT language FROM users WHERE chat_id = ?"""
+    language = fetch(fetch_language_query, (callback.message.chat.id,))[0][0]
+
+    _ = get_translation(language)  # alias for the function
+
+    await callback.answer(_("ready"))
+
+
+@router.callback_query(F.data == "english")
+async def set_english_language(callback: types.CallbackQuery) -> None:
+    """method that sets the English language for a user in the database"""
+
+    update(message=callback.message, language="en")
+
+    # query to get user language from database
+    fetch_language_query = """SELECT language FROM users WHERE chat_id = ?"""
+    language = fetch(fetch_language_query, (callback.message.chat.id,))[0][0]
+
+    _ = get_translation(language)  # alias for the function
+
+    await callback.answer(_("ready"))
