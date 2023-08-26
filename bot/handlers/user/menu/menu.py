a8164e1df20bb68e394878fb81f5ff7a63f1667c
Andrey Sadkov <ad.sadkov@gmail.com>
2023-08-27 00:06:42 +0300

    fix: the button responsible for returning to the menu did not work

--- bot/handlers/user/menu/menu.py
+++ bot/handlers/user/menu/menu.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Router, types
+from aiogram import Bot, Router, F, types
 from aiogram.filters import Command
 
 from bot.misc.show_menu import show_menu
@@ -11,3 +11,10 @@ async def menu(message: types.Message, bot: Bot) -> None:
     """method that sends the bot menu to the user on command"""
 
     await show_menu(bot=bot, data=message)
+
+
+@router.callback_query(F.data == "menu")
+async def menu(callback: types.CallbackQuery, bot: Bot) -> None:
+    """method that changes the bot's last message to a menu"""
+
+    await show_menu(bot=bot, data=callback)
