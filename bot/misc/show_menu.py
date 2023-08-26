0dd1b2d3ca59523a09169e6cbf9b46222387b2c0
Andrey Sadkov <ad.sadkov@gmail.com>
2023-08-26 22:56:53 +0300

    refactor: replaced variable names with more correct ones

--- bot/misc/show_menu.py
+++ bot/misc/show_menu.py
@@ -12,26 +12,26 @@ async def show_menu(bot: Bot, data: Union[types.CallbackQuery, types.Message]) -
 
     try:
         if isinstance(data, types.CallbackQuery):
-            user_id = data.from_user.id
+            chat_id = data.from_user.id
             first_name = data.from_user.first_name
             message_id = data.message.message_id
         elif isinstance(data, types.Message):
-            user_id = data.from_user.id
+            chat_id = data.from_user.id
             first_name = data.from_user.first_name
 
         # query to get user language from database
         fetch_language_query = "SELECT language FROM users WHERE chat_id = ?"
-        language = fetch(fetch_language_query, (user_id,))[0][0]
+        language = fetch(fetch_language_query, (chat_id,))[0][0]
 
         _ = get_translation(language)  # alias for the function
 
         if isinstance(data, types.CallbackQuery):
-            await bot.edit_message_text(chat_id=user_id, message_id=message_id,
+            await bot.edit_message_text(chat_id=chat_id, message_id=message_id,
                                         text=_("hello, {first_name}").format(first_name=first_name),
-                                        reply_markup=menu_keyboard(user_id))
+                                        reply_markup=menu_keyboard(chat_id))
         elif isinstance(data, types.Message):
             await data.answer(text=_("hello, {first_name}").format(first_name=first_name),
-                              reply_markup=menu_keyboard(user_id))
+                              reply_markup=menu_keyboard(chat_id))
 
     except Exception as e:
         print(f"An error has occurred: {e}")
