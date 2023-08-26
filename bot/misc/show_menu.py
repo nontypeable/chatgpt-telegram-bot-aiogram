8aa38dea1a29599217f83bd4830ff91afe6d7065
Andrey Sadkov <ad.sadkov@gmail.com>
2023-08-26 12:47:48 +0300

    add: a method that shows the menu to the user

---
+++ bot/misc/show_menu.py
@@ -0,0 +1,37 @@
+from typing import Union
+
+from aiogram import Bot, types
+
+from bot.db.methods.fetch import fetch
+from bot.keyboards.inline.menu.menu import menu_keyboard
+from bot.misc.get_translation import get_translation
+
+
+async def show_menu(bot: Bot, data: Union[types.CallbackQuery, types.Message]) -> None:
+    """method that shows the menu"""
+
+    try:
+        if isinstance(data, types.CallbackQuery):
+            user_id = data.from_user.id
+            first_name = data.from_user.first_name
+            message_id = data.message.message_id
+        elif isinstance(data, types.Message):
+            user_id = data.from_user.id
+            first_name = data.from_user.first_name
+
+        # query to get user language from database
+        fetch_language_query = "SELECT language FROM users WHERE chat_id = ?"
+        language = fetch(fetch_language_query, (user_id,))[0][0]
+
+        _ = get_translation(language)  # alias for the function
+
+        if isinstance(data, types.CallbackQuery):
+            await bot.edit_message_text(chat_id=user_id, message_id=message_id,
+                                        text=_("hello, {first_name}").format(first_name=first_name),
+                                        reply_markup=menu_keyboard(user_id))
+        elif isinstance(data, types.Message):
+            await data.answer(text=_("hello, {first_name}").format(first_name=first_name),
+                              reply_markup=menu_keyboard(user_id))
+
+    except Exception as e:
+        print(f"An error has occurred: {e}")
