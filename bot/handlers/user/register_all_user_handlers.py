07ce478a44fcfb5f503534852035de6eb9959e8b
Andrey Sadkov <ad.sadkov@gmail.com>
2023-08-27 12:50:59 +0300

    fix: the user did not get information when entering the /info command

--- bot/handlers/user/register_all_user_handlers.py
+++ bot/handlers/user/register_all_user_handlers.py
@@ -1,8 +1,9 @@
 from aiogram import Dispatcher
 
-from bot.handlers.user.start import router as start_router
-from bot.handlers.user.settings.language import router as language_settings_router
+from bot.handlers.user.menu.information import router as information_router
 from bot.handlers.user.menu.menu import router as menu_router
+from bot.handlers.user.settings.language import router as language_settings_router
+from bot.handlers.user.start import router as start_router
 
 
 def register_all_user_handlers(dp: Dispatcher) -> None:
@@ -11,3 +12,4 @@ def register_all_user_handlers(dp: Dispatcher) -> None:
     dp.include_router(start_router)
     dp.include_router(language_settings_router)
     dp.include_router(menu_router)
+    dp.include_router(information_router)
