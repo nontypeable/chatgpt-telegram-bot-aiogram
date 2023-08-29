06ba98b8ab5720f18a7c5c0941dab709db957866
Andrey <ad.sadkov@gmail.com>
2023-08-29 17:03:10 +0300

    fix: path to bot locales

--- bot/misc/get_translation.py
+++ bot/misc/get_translation.py
@@ -7,9 +7,8 @@ def get_translation(language: str) -> Callable[..., str]:
     """method that makes it easier to work with multiple locales"""
 
     # defining an absolute path to locales
-    locales_path = os.path.join(os.path.dirname(__file__), "locales")
+    locales_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "locales")
 
     translation = gettext.translation(domain="base", localedir=locales_path, languages=[language], fallback=True)
 
     return translation.gettext
-
